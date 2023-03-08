164d19dd16ea52540a2b0812e68519b59b10279e
avagce <avagce@gmail.com>
2023-03-08 13:29:16 +0900

    apitest ing

--- src/main/java/com/springboot/tourvisit/api/apicontroller.java
+++ src/main/java/com/springboot/tourvisit/api/apicontroller.java
@@ -14,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -29,11 +32,11 @@ import org.springframework.web.client.RestTemplate;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-import jdk.nashorn.internal.parser.JSONParser;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@RestController
+@Controller
 public class apicontroller{
 	@RequestMapping("apitest.do")
 	public String apitest(){
@@ -42,7 +45,7 @@ public class apicontroller{
 		
 	
 	@RequestMapping("api.do")
-public void callDetail(HttpServletRequest request, HttpServletResponse response) throws IOException{
+public void callDetail(HttpServletRequest request, HttpServletResponse response) throws IOException, Exception{
 			
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
@@ -87,6 +90,58 @@ public void callDetail(HttpServletRequest request, HttpServletResponse response)
         System.out.println(s);
         
         
+        
+        JSONParser parser = new JSONParser();
+      
+        
+        Object obj = parser.parse(s);
+        System.out.println(obj.toString()); // JSON 전체
+        JSONObject jsonbody = (JSONObject)obj; //json object 로 변경
+       JSONObject jsonbody2 = (JSONObject) jsonbody.get("response");
+       JSONObject jsonbody3 = (JSONObject) jsonbody2.get("body");
+       JSONObject jsonbody4 = (JSONObject) jsonbody3.get("items");
+     //  JSONObject jsonbody5 = (JSONObject) jsonbody4.get("item"); 실패
+       // System.out.println("Body만 나오는지 확인 " +jsonbody3); // body 안에 pageNo,totalCount,items
+      // System.out.println("items만 나오는지 확인 " +jsonbody4); // items 출력 확인
+     //   System.out.println("item만 나오는지 확인 " +jsonbody5); // item은 출력 안됨
+        /*
+        JSONObject jsonMain = (JSONObject)obj; //전체 json
+       Object objsecond = jsonMain.get("body");
+       JSONObject jsonSecond = (JSONObject)objsecond;
+        Object objThird = jsonSecond.get("items");
+        JSONObject jsonThird = (JSONObject)objThird;
+        
+        
+        
+       /* 
+        if(jsonArr.size() > 0) {
+        	for(int i=0; i<jsonArr.size();i++) {
+        		JSONObject jsonObj = (JSONObject)jsonArr.get(i);
+        		System.out.println((String)jsonObj.get("items.item.contenttypeid"));
+        		/*System.out.println((String)jsonObj.get("booktour"));
+        		System.out.println((String)jsonObj.get("createdtime"));
+        		System.out.println((String)jsonObj.get("homepage"));
+        		System.out.println((String)jsonObj.get("modifiedtime"));
+        		System.out.println((String)jsonObj.get("tel"));
+        		System.out.println((String)jsonObj.get("telname"));
+        		System.out.println((String)jsonObj.get("title"));
+        		System.out.println((String)jsonObj.get("firstimage"));
+        		System.out.println((String)jsonObj.get("firstimage2"));
+        		System.out.println((String)jsonObj.get("areacode"));
+        		System.out.println((String)jsonObj.get("sigungucode"));
+        		System.out.println((String)jsonObj.get("cat1"));
+        		System.out.println((String)jsonObj.get("cat2"));
+        		System.out.println((String)jsonObj.get("cat3"));
+        		System.out.println((String)jsonObj.get("addr1"));
+        		System.out.println((String)jsonObj.get("addr2"));
+        		System.out.println((String)jsonObj.get("zipcode"));
+        		System.out.println((String)jsonObj.get("mapx"));
+        		System.out.println((String)jsonObj.get("mapy"));
+        		System.out.println((String)jsonObj.get("mlevel"));
+        		System.out.println((String)jsonObj.get("overview"));
+        		System.out.println((String)jsonObj.get("contentid"));
+        	}
+        }*/
         
        
        

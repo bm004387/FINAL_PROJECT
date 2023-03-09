08d91a37afabfc35d8cc46284480433dbe801f78
bm004387 <bm004387@gmail.com>
2023-03-09 09:19:48 +0900

    boardpackge name update

--- src/main/java/com/springboot/tourvisit/board/model/BoardVO.java
+++ src/main/java/com/springboot/tourvisit/board/model/BoardVO.java
@@ -1,4 +1,4 @@
-package com.springboot.tourvisit.model;
+package com.springboot.tourvisit.board.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -23,18 +23,18 @@ import lombok.Setter;
 @Table(name = "SPRINGBOOT_CRUD") // 테이블의 이름을 설정 (SPRINGBOOT_CRUD)
 @Getter // @Getter, @Setter : getter과 setter을 자동으로 만들어주는 Lombok 애노테이션 설정
 @Setter		
-public class ArticleVO {
+public class BoardVO {
 
 	@Id // PK 설정
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, // 시퀀스 생성기를 통해 시퀀스 만들고 ID에 자동으로 주입
 					generator = "IDX_SEQ_GEN")
 	
-	@Column(name = "ARTICLE_ID")
+	@Column(name = "BOARD_ID")
 	private int id;
 	
-	@Column(name = "ARTICLE_TITLE")
+	@Column(name = "BOARD_TITLE")
 	private String title;
 	
-	@Column(name = "ARTICLE_CONTENT")
+	@Column(name = "BOARD_CONTENT")
 	private String content;
 }

450ec9db31b2875362a4a3237820a4e37e263345
bm004387 <bm004387@gmail.com>
2023-03-21 17:36:30 +0900

    reactBoard CRUD success

--- src/main/java/com/springboot/tourvisit/react/repository/ReactBoardRepository.java
+++ src/main/java/com/springboot/tourvisit/react/repository/ReactBoardRepository.java
@@ -1,11 +1,25 @@
 package com.springboot.tourvisit.react.repository;
 
-import java.time.LocalDate;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.tourvisit.react.model.ReactBoard;
 
 public interface ReactBoardRepository extends JpaRepository<ReactBoard, Integer>{
-	
+
+//	public final static String SELECT_BOARD_LIST_PAGED = ""
+//			+ "SELECT "
+//			+ "rt_bno,"
+//			+ "rt_title,"
+//			+ "rt_content,"
+//			+ "rt_writer,"
+//			+ "rt_regdate,"
+//			+ "rt_updatedate,"
+//			+ "counts"
+//			+ " FROM rt_board WHERE rownum >= ?1 and rownum <= ?2 order by rt_bno desc ";
+//	
+//	
+//	@Query(value = SELECT_BOARD_LIST_PAGED, nativeQuery = true)
+//	List<ReactBoard> findFromTo(
+//			final Integer objectStartNum,
+//			final Integer objectEndNum);
 }
--- src/main/java/com/springboot/tourvisit/react/service/ReactBoardService.java
+++ src/main/java/com/springboot/tourvisit/react/service/ReactBoardService.java
@@ -1,7 +1,9 @@
 package com.springboot.tourvisit.react.service;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,32 @@ public class ReactBoardService {
 	@Autowired
 	private ReactBoardRepository reactBoardRepository;
 	
+//	public int findAllCount() {
+//		return (int) reactBoardRepository.count();
+//	}
+	
+//	public ResponseEntity<Map> getPagingBoard(Integer p_num) {
+//		Map result = null;
+//		
+//		PagingUtil pu = new PagingUtil(p_num, 10, 5); // ($1:표시할 현재 페이지, $2:한페이지에 표시할 글 수, $3:한 페이지에 표시할 페이지 버튼의 수 )
+//		List<ReactBoard> list = reactBoardRepository.findFromTo(pu.getObjectStartNum(), pu.getObjectCountPerPage());
+//		pu.setObjectCountTotal(findAllCount());
+//		pu.setCalcForPaging();
+//		
+//		System.out.println("p_num : "+p_num);
+//		System.out.println(pu.toString());
+//		
+//		if (list == null || list.size() == 0) {
+//			return null;
+//		}
+//		
+//		result = new HashMap<>();
+//		result.put("pagingData", pu);
+//		result.put("list", list);
+//		
+//		return ResponseEntity.ok(result);
+//	}	
+	
 	public List<ReactBoard> getAllBoard(){
 		return reactBoardRepository.findAll();
 	}
@@ -35,4 +63,24 @@ public class ReactBoardService {
 		return ResponseEntity.ok(reactBoard);
 	}
 
+	public ResponseEntity<ReactBoard> updateBoard(Integer bno, ReactBoard updateBoard) {
+		ReactBoard reactBoard = reactBoardRepository.findById(bno).orElseThrow(() -> new ResourceNotFoundException("Not exist Board Data by no : ["+bno+"]"));
+		reactBoard.setTitle(updateBoard.getTitle());
+		reactBoard.setContent(updateBoard.getContent());
+		reactBoard.setWriter(updateBoard.getWriter());
+		reactBoard.setUpdateDate(LocalDate.now());
+		
+		ReactBoard endUpdateBoard = reactBoardRepository.save(reactBoard);
+		return ResponseEntity.ok(endUpdateBoard);
+	}
+
+	public ResponseEntity<Map<String, Boolean>> deleteBoard(Integer bno) {
+		ReactBoard reactBoard = reactBoardRepository.findById(bno)
+				.orElseThrow(() -> new ResourceNotFoundException("Not exist Board Data by no : ["+bno+"]"));
+		
+		reactBoardRepository.delete(reactBoard);
+		Map<String, Boolean> response = new HashMap<String, Boolean>();
+		response.put("Deleted Board Data by id : ["+bno+"]", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
 }
